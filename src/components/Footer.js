fcc99f01001be643cbe0453c65a70f95aedb57d2
richieArnold <arnoldrichie31.rar@gmail.com>
2023-11-02 07:41:08 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Crud Operation Demonstration</h1>
+      <p style={{ textAlign: "center" }}>
+        This project demonstrates crud operation using MERN technology
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/ViewTableRow.js
@@ -0,0 +1,41 @@
+import React from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function ViewTableRow(props) {
+  const DeleteEmployee = () => {
+    const url =
+      "https://companymangament.onrender.com/employees/delete-employee/";
+    axios
+      .delete(url + props.obj._id)
+      .then((res) => {
+        if (res.status === 200) {
+          alert("Employee Deleted");
+          window.location.reload();
+        } else {
+          Promise.reject();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <tr>
+      <td>{props.obj.name}</td>
+      <td>{props.obj.phone}</td>
+      <td>{props.obj.email}</td>
+      <td>{props.obj.password}</td>
+      <td>
+        <Link to={`/edit-employee/${props.obj._id}`}>
+          <button id="action">Edit</button>
+        </Link>
+        <button id="action" onClick={DeleteEmployee}>
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+}
+
+export default ViewTableRow;
